973d1de4d2d5cc30f7b489c93d1d772ef77a9be3
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-05-01 16:21:12 -0500

    Cambios en App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,60 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/inicio">INICIO</router-link>  |  
+    <router-link to="/contactenos">CONTACTENOS</router-link>  |  
+    <router-link to="/servicios">SERVICIOS</router-link>  |  
+    <router-link to="/productos">PRODUCTOS</router-link>  |  
+    <router-link to="/nosotros">NOSOTROS</router-link>  |  
   </nav>
-  <router-view/>
+  <router-view class="con"/>
+  <footer>
+    <FooterCom/>
+  </footer>
 </template>
 
-<style>
+<style scoped >
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #363232;
 }
-
+.con {
+  padding-block-start: 6rem;
+}
 nav {
   padding: 30px;
+  background: #a4c7eb;
+  color: black;
+  letter-spacing: 3px;
+  text-align: center;
+
+  position: fixed;
+  left: 0;
+  top: 0;
+  right: 0;
+
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  padding: 35px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #0a0a0a;
+  
 }
 </style>
+<script lang="ts">
+import FooterCom from '@/components/FooterCom.vue';
+
+export default {
+  components: {
+    FooterCom,
+  },
+}
+
+</script>
+  
