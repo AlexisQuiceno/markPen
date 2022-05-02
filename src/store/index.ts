a9dae22a05ba07fa5db9dd7257d7f91d9dc0c2ac
ElSebastianLopez <sebaslopezhenao@gmail.com>
2022-05-01 20:39:30 -0500

    agregue imagenes

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,8 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     cards: [
-      {titulo: 'titulo 1', description:'Lorem ipsum....', imagen:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.lp2VZXMe35-M1YF57kYJVQHaE7%26pid%3DApi&f=1'},
-      {titulo:'titulo 2', description:'Lorem ipsum....', imagen:'wsl$/Ubuntu/root/NuevoProyectoProsper/markPen/src/assets/bosque1.jpeg'},
+      {titulo: 'titulo 1', description:'Lorem ipsum....', imagen:''},
+      {titulo:'titulo 2', description:'Lorem ipsum....', imagen:''},
       {titulo: 'titulo 3', description:'Lorem ipsum....', imagen:''},
       {titulo:'titulo 4', description:'Lorem ipsum....'},
       {titulo: 'titulo 5', description:'Lorem ipsum....'},
@@ -18,6 +18,7 @@ export default createStore({
     
   },
   getters: {
+
   },
   mutations: {
   },
