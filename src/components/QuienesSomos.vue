8cc356a7d128733ae1ef4a6982901b72a17830f4
AlexisQuiceno <alexisquicenorodriguez@gmail.com>
2022-05-01 20:35:01 -0500

    require para las imagenes en el for

--- src/components/QuienesSomos.vue
+++ src/components/QuienesSomos.vue
@@ -7,11 +7,33 @@
 			<div class="container flex flex-col items-center px-5 py-16 mx-auto md:flex-row lg:px-28">
 				<div class="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:mb-0">
 					<h1 class="mb-8 text-2xl font-black tracking-tighter text-black md:text-5xl title-font"> Por que nuestro proyecto. </h1>
-					<p class="mb-8 text-base leading-relaxed text-left text-blueGray-600 "> Deploy your mvp in minutes, not days. WT offers you a a wide selection swapable sections for your landing page. </p>
+					<p class="mb-8 text-base leading-relaxed text-left text-blueGray-600 ">
+						Aqui Encontraras un lugar en donde enterarte de las mejores noticias del municipio, y de los lugares ideales para pasar un reto agradable o acquirir tus productos favoritos. </p>
 					<div class="flex flex-col justify-center lg:flex-row">
-						<button class="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"> Show me </button>
-						<p class="mt-2 text-sm text-left text-blueGray-600 md:ml-6 md:mt-0"> It will take you to candy shop. <br class="hidden lg:block">
-						<a href="#" class="inline-flex items-center font-semibold text-blue-600 md:mb-2 lg:mb-0 hover:text-black " title="read more"> Read more about it » </a>
+						<button class="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"> Ver mas. </button>
+						<p class="mt-2 text-sm text-left text-blueGray-600 md:ml-6 md:mt-0"> Conoce mas de nosotros... <br class="hidden lg:block">
+						<a href="#" class="inline-flex items-center font-semibold text-blue-600 md:mb-2 lg:mb-0 hover:text-black " title="read more"> Leer mas» </a>
+						</p>
+					</div>
+				</div>
+				<div class="w-full lg:w-1/3 lg:max-w-lg md:w-1/2">
+					<img class="object-cover object-center rounded-lg " alt="hero" src="../assets/nochePensilvania.jpeg">
+				</div>
+			</div>
+		</section>
+	</div>
+
+		<div class="drop-shadow-md9">
+		<section class="text-blueGray-700 bg-white mt-20 ">
+			<div class="container flex flex-col items-center px-5 py-16 mx-auto md:flex-row lg:px-28">
+				<div class="flex flex-col items-start mb-16 text-left lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:mb-0">
+					<h1 class="mb-8 text-2xl font-black tracking-tighter text-black md:text-5xl title-font"> Por que nuestro proyecto. </h1>
+					<p class="mb-8 text-base leading-relaxed text-left text-blueGray-600 ">
+						Aqui Encontraras un lugar en donde enterarte de las mejores noticias del municipio, y de los lugares ideales para pasar un reto agradable o acquirir tus productos favoritos. </p>
+					<div class="flex flex-col justify-center lg:flex-row">
+						<button class="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-blue-600 rounded-lg hover:bg-blue-700 focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2"> Ver mas. </button>
+						<p class="mt-2 text-sm text-left text-blueGray-600 md:ml-6 md:mt-0"> Conoce mas de nosotros... <br class="hidden lg:block">
+						<a href="#" class="inline-flex items-center font-semibold text-blue-600 md:mb-2 lg:mb-0 hover:text-black " title="read more"> Leer mas» </a>
 						</p>
 					</div>
 				</div>
@@ -20,21 +42,6 @@
 				</div>
 			</div>
 		</section>
-	</div>
-    <div class="flex flex-col justify-center h-screen">
-		<div class="flex flex-col md:flex-row md:space-x-5 space-y-3 md:space-y-0 rounded-xl shadow-lg p-3 max-w-xs md:max-w-3xl mx-auto border border-white bg-white">
-			<div class="w-full md:w-1/3 bg-white grid place-items-center">
-			<img src="https://images.pexels.com/photos/4381392/pexels-photo-4381392.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" alt="tailwind logo" class="rounded-xl" />
-			</div>
-
-			<div class="w-full md:w-2/3 bg-white flex flex-col space-y-2 p-3">
-				<div class="flex justify-between item-center">
-				</div>
-				<h3 class="font-black text-gray-800 md:text-3xl text-xl">The Majestic and Wonderful Bahamas</h3>
-				<p class="md:text-lg text-gray-500 text-base">The best kept secret of The Bahamas is the country’s sheer
-					size and diversity. With 16 major islands, The Bahamas is an unmatched destination</p>
-			</div>
-		</div>
 	</div>
 </div>
 </template>
--- src/views/NosotrosMark.vue
+++ src/views/NosotrosMark.vue
@@ -1,19 +1,26 @@
 <template>
-    <div>
+    <div class="margenes">
         <QuienesSomos/>
+        <NuestroEquipo/>
     </div>
 </template>
 
 <script lang="ts">
 import QuienesSomos from '@/components/QuienesSomos.vue';
+import NuestroEquipo from '@/components/NuestroEquipo.vue';
 
 export default {
   components: {
     QuienesSomos,
+    NuestroEquipo,
   },
 }
 
 </script>
 <style scoped>
 
+  .margenes {
+    padding-top: 1%;
+    padding-bottom: 2%;
+  }
 </style>
